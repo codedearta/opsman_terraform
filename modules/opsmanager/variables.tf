5bd6419f5cb6f19ad04bb30409d2a6975200ff4f
dearta <sepp.renfer@gmail.com>
2023-06-28 07:56:58 +0100

    Update variables.tf updated the latest Amzon Linux 2 AMI

--- modules/opsmanager/variables.tf
+++ modules/opsmanager/variables.tf
@@ -1,4 +1,4 @@
-variable "ami" { default = "ami-0274e11dced17bb5b" }
+variable "ami" { default = "ami-0094635555ed28881" }
 variable "instance_type" { default = "t2.xlarge" }
 variable "key_name" { default = "sepp_renfer_awskey_mongodb" }
 variable "private_key" {}
